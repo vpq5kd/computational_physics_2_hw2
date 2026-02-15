4c7db2ecec3cc87cd7cb2d20412801cc428e0f1b
sophia spaner <vpq5kd@virginia.edu>
2026-02-14 23:09:58 -0500

    adding specific heat

--- ising.py
+++ ising.py
@@ -15,38 +15,49 @@ def transition_probability(delta_energy,n,T):
     return (1/n)*np.min([1, np.exp(-delta_energy/T)])
 
 def metropolis(n):
-    temp_array = np.linspace(1e-6, 5.1, 1000)
+    temp_array = np.linspace(0.1, 5.1, 1000)[::-1]
     energy_array = []
+    energy_array_squared = []
     magnetization_array = [] 
     
     ising_start_state = np.random.choice([-1,1], size=n)
     
     state = ising_start_state
     for T in temp_array:
-        index_to_flip = rng.integers(n)
-        new_state = state.copy()
-        new_state[index_to_flip] = -new_state[index_to_flip]
+        temp_energy_array = []
+        for i in range(0, 10000):
+            index_to_flip = rng.integers(n)
+            new_state = state.copy()
+            new_state[index_to_flip] = -new_state[index_to_flip]
 
-        starting_state_magnetization = magnetization(state)
-        starting_state_energy = energy(state, n) 
-        new_state_energy = energy(new_state, n)
-        delta_energy = new_state_energy - starting_state_energy
-        probability = transition_probability(delta_energy, n, T)
+            starting_state_energy = energy(state, n) 
+            new_state_energy = energy(new_state, n)
+            delta_energy = new_state_energy - starting_state_energy
+            probability = transition_probability(delta_energy, n, T) 
+            
+            accept = rng.random() < probability
+            if accept: 
+                state = new_state
+            temp_energy_array.append(energy(state,n)/n)
+        
+        temp_energy_array_np = np.array(temp_energy_array)
+        energy_array.append(np.mean(temp_energy_array_np))
+        energy_array_squared.append((np.mean(temp_energy_array_np**2)))
+        magnetization_array.append(magnetization(state))
 
-        energy_array.append(starting_state_energy)
-        magnetization_array.append(starting_state_magnetization)
-       
-        accept = rng.random() < probability
-        if accept: 
-            state = new_state
-    return temp_array, energy_array, magnetization_array
+    return temp_array, energy_array, energy_array_squared, magnetization_array
 
 def main():
     N = 50
-    temp_array, energy_array, magnetization_array = metropolis(N)
-    
+    temp_array, energy_array, energy_array_squared, magnetization_array = metropolis(N)
+  
+    energy_array_np = np.array(energy_array)
+    energy_array_squared_np = np.array(energy_array_squared)
+    specific_heat_array = (energy_array_squared_np - energy_array_np**2)/N*temp_array
     plt.figure()
-    plt.plot(temp_array, energy_array, 'o')
+    fig, ax = plt.subplots(1,3)
+    ax[0].plot(temp_array, energy_array, 'o')
+    ax[1].plot(temp_array, specific_heat_array, 'o')
     plt.show()
 
 main()
